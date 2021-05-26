0c002b28efb11cc4d09b725e51f54b96c15ef8e0
huang825172 <1048035187@qq.com>
2021-05-26 09:48:35 +0800

    add: gold indicator

---
+++ Assets/Script/Controller/GoldIndicatorController.cs
@@ -0,0 +1,57 @@
+﻿using Script.Networking.Game;
+using UnityEngine;
+
+namespace Script.Controller
+{
+    public class GoldIndicatorController : MonoBehaviour
+    {
+        public int index;
+        public Material off;
+        public Material on;
+
+        private bool _activated;
+        private float _startTime;
+
+        private void FixedUpdate()
+        {
+            var gm = FindObjectOfType<GameManager>();
+            if (gm)
+            {
+                if (gm.playing)
+                {
+                    if (gm.countDown <= 408 && gm.countDown >= 406)
+                    {
+                        if (index == 2 || index == 4)
+                        {
+                            if (!_activated)
+                            {
+                                _activated = true;
+                                _startTime = Time.time;
+                            }
+
+                            for (var i = 0; i < transform.childCount; i++)
+                                transform.GetChild(i).GetComponent<MeshRenderer>().material =
+                                    Mathf.Sin((Time.time - _startTime) * 18) > 0 ? on : off;
+                        }
+                    }
+
+                    if (gm.countDown <= 243 && gm.countDown >= 241)
+                    {
+                        if (index == 1 || index == 3 || index == 5)
+                        {
+                            if (!_activated)
+                            {
+                                _activated = true;
+                                _startTime = Time.time;
+                            }
+
+                            for (var i = 0; i < transform.childCount; i++)
+                                transform.GetChild(i).GetComponent<MeshRenderer>().material =
+                                    Mathf.Sin((Time.time - _startTime) * 18) > 0 ? on : off;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Script/Controller/GuardController.cs
+++ Assets/Script/Controller/GuardController.cs
@@ -175,7 +175,7 @@ namespace Script.Controller
                         delta *= 10;
                         delta.y /= Screen.height;
                         delta.x /= Screen.width;
-                        var noise = Random.Range(-0.12f, 0.12f);
+                        var noise = Random.Range(-0.05f, 0.05f);
                         delta += new Vector3(noise, noise, 0);
                         _pitchingSpeed -= 1.0f / (1 + Mathf.Pow((float) Math.E, -delta.y)) - 0.5f;
                         _steeringSpeed += 1.0f / (1 + Mathf.Pow((float) Math.E, -delta.x)) - 0.5f;
@@ -184,7 +184,7 @@ namespace Script.Controller
                         {
                             smallAmmo--;
                             Fire(0);
-                            _fireCd = Random.Range(8, 16);
+                            _fireCd = Random.Range(6, 12);
                         }
                         else
                         {
